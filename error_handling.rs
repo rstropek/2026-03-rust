695e8e398e7a8f28cd75e8b7729ccf685e955a45
Rainer Stropek <rainer@software-architects.at>
2026-04-01 12:34:09 +0200

    Add sample for error handling (with anyhow)

---
+++ error_handling.rs
@@ -0,0 +1,46 @@
+use serde_json::Value;
+use tokio::{fs::File, io::AsyncReadExt};
+
+async fn read_and_parse(file_name: &str) -> String {
+    let mut f = File::open(file_name).await.unwrap();
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await.unwrap();
+    let s = String::from_utf8(buf).unwrap();
+
+    let v: Value = serde_json::from_str(s.as_str()).unwrap();
+    v.get("connectionString").unwrap().to_string()
+}
+
+async fn read_and_parse_2(file_name: &str) -> Result<String, &str> {
+    let mut f = File::open(file_name).await.map_err(|_| "failed to open file")?; // error propagation
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await.map_err(|_| "failed to read file")?;
+    let s = String::from_utf8(buf).map_err(|_| "failed to parse UTF-8")?;    
+    
+    let v: Value = serde_json::from_str(s.as_str()).map_err(|_| "failed to parse JSON")?;
+    match v.get("connectionString") {
+        Some(value) => Ok(value.to_string()),
+        None => Err("connectionString not found in JSON"),
+    }
+}
+
+async fn read_and_parse_3(file_name: &str) -> anyhow::Result<String> {
+    let mut f = File::open(file_name).await?; // error propagation
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await?;
+    let s = String::from_utf8(buf)?;    
+    
+    let v: Value = serde_json::from_str(s.as_str())?;
+    match v.get("connectionString") {
+        Some(value) => Ok(value.to_string()),
+         None => Err(anyhow::anyhow!("connectionString not found in JSON")),    }
+}
+
+#[tokio::main]
+async fn main() {
+    let connection_string = read_and_parse_3("config.json").await;
+    match connection_string {
+        Ok(conn_str) => println!("Connection string: {}", conn_str),
+        Err(e) => eprintln!("Error: {}", e), // Custom error handling, NO PANIC!
+    }
+}
